629334dd4d26827e7b358c6b62076c1a7b31aa8a
locpython <138984694+locpython@users.noreply.github.com>
2024-03-07 22:41:06 +0700

    Hello.py

--- Hello.py
+++ Hello.py
@@ -3,9 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 st.title("Revenue Prediction")
-st.balloons()
-st.snow()
-pred = st.slider('Input Temperature', 0, 45, 25)
+pred = st.number_input('Input Temperature', step = 1)
 # The correct direct download URL
 file_id = '1wZETgqcnl0dQJ8FV3hI_Y64LK1KNfGyE'
 direct_link = f'https://drive.google.com/uc?export=download&id={file_id}'
@@ -18,4 +16,7 @@ model = LinearRegression()
 model.fit(x, y)
 y_pred = model.predict([[pred]])
 k = y_pred[0][0]
-st.write(f"Revenue Prediction: {round(k,1)}")
+if st.button('Predict', type="primary"):
+    st.balloons()
+    st.snow()
+    st.write(f"Revenue Prediction: {round(k,1)}")
